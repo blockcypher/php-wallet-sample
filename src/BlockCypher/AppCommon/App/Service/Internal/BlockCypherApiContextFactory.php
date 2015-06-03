dfa181db09d89d64a33425fdf0b5ed484117a6f2
Jose Celano <josecelano@gmail.com>
2015-06-03 11:56:27 +0100

    BlockCypher log file moved to symfony logs folder

--- src/BlockCypher/AppCommon/App/Service/Internal/BlockCypherApiContextFactory.php
+++ src/BlockCypher/AppCommon/App/Service/Internal/BlockCypherApiContextFactory.php
@@ -47,7 +47,7 @@ class BlockCypherApiContextFactory extends ApiContext
         $config = array(
             'mode' => 'sandbox',
             'log.LogEnabled' => true,
-            'log.FileName' => '../BlockCypher.log',
+            'log.FileName' => '../app/logs/BlockCypher.log',
             'log.LogLevel' => 'DEBUG', // PLEASE USE `INFO` LEVEL FOR LOGGING IN LIVE ENVIRONMENTS
             'validation.level' => 'log',
             // 'http.CURLOPT_CONNECTTIMEOUT' => 30
